76a433c0552c508727067acc3db144ceef864d07
wags83 <gpwagner@gmail.com>
2020-03-09 16:25:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file
+
+function findMatching(list, name) {
+  return list.filter(function(driverName) {
+    return driverName.toLowerCase() === name.toLowerCase()
+  })
+}
+
+function fuzzyMatch(list, name) {
+    return list.filter(function (driverName){
+        return driverName[0].toLowerCase() === name[0].toLowerCase()
+    })
+    
+}
+
+function matchName(list, name) {
+    return list.filter(function (driverObject){
+        return driverObject["name"].toLowerCase() === name.toLowerCase()
+    })
+    
+}
